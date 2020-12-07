1efcbd4ea2c5e9f528930e014c3a3cdbcaad8037
Salvador Di Renzo <direnzosalvatore@gmail.com>
2020-12-07 17:49:40 -0400

    fix follow bad calls

--- app/services/follow_service.rb
+++ app/services/follow_service.rb
@@ -10,7 +10,7 @@ class FollowService < BaseService
       follow = Follow.new(follow_params)
       raise ActiveRecord::RecordInvalid.new(follow) unless follow.valid?
       raise PersonalizedException.new("User cannot follows itself", :bad_request) if @current_user.id == params[:followed_id]
-      raise PersonalizedException.new("User has been already followed", :bad_request) if self.exists?(follow_params).present?
+      raise PersonalizedException.new("User has been already followed", :bad_request) if Follow.exists?(follow_params).present?
       follow.save!
       { content: { follow_id: follow.id } }
     end
