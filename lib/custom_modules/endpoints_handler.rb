30c13f2a03220cd039471f34872b286514fe2bea
Salvador Di Renzo <direnzosalvatore@gmail.com>
2020-12-07 17:09:51 -0400

    fix EndpointsHandler for instance methods

--- lib/custom_modules/endpoints_handler.rb
+++ lib/custom_modules/endpoints_handler.rb
@@ -1,39 +1,36 @@
 module EndpointsHandler
-  module ClassMethods
-    COMMON_ERRORS = [
-      ActiveRecord::RecordNotFound,
-      ActiveRecord::RecordInvalid,
-      ActiveRecord::RecordNotUnique
-    ]
+  
+  COMMON_ERRORS = [
+    ActiveRecord::RecordNotFound,
+    ActiveRecord::RecordInvalid,
+    ActiveRecord::RecordNotUnique
+  ]
 
-    # Used for non token required endpoints
-    def flat_endpoint(*pars)
-      begin
-        [yield(*pars), :ok]
-      rescue PersonalizedException => e
-        [{ error: e&.message }, e&.status_code]
-      rescue *COMMON_ERRORS => e
-        [{ error: e&.message }, :unprocessable_entity]
-      rescue StandardError => e
-        [{ error: e&.message }, :internal_server_error]
-      end
-    end
-
-    # Used for permissions required endpoints
-    def permissions_endpoint(user, *pars, &block)
-      # Here can be putted code to verify if endpoint if accesible by user
-      begin
-        [yield(user, *pars), :ok]
-      rescue PersonalizedException => e
-        [{ error: e&.message }, e&.status_code]
-      rescue *COMMON_ERRORS => e
-        [{ error: e&.message }, :unprocessable_entity]
-      rescue StandardError => e
-        [{ error: e&.message }, :internal_server_error]
-      end
+  # Used for non token required endpoints
+  def flat_endpoint(*pars)
+    begin
+      [yield(*pars), :ok]
+    rescue PersonalizedException => e
+      [{ error: e&.message }, e&.status_code]
+    rescue *COMMON_ERRORS => e
+      [{ error: e&.message }, :unprocessable_entity]
+    rescue StandardError => e
+      [{ error: e&.message }, :internal_server_error]
     end
   end
-  def self.included(receiver)
-    receiver.extend ClassMethods
+
+  # Used for permissions required endpoints
+  def permissions_endpoint(user, *pars, &block)
+    # Here can be putted code to verify if endpoint if accesible by user
+    begin
+      [yield(user, *pars), :ok]
+    rescue PersonalizedException => e
+      [{ error: e&.message }, e&.status_code]
+    rescue *COMMON_ERRORS => e
+      [{ error: e&.message }, :unprocessable_entity]
+    rescue StandardError => e
+      [{ error: e&.message }, :internal_server_error]
+    end
   end
+
 end
