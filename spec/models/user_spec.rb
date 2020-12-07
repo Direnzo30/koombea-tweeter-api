b08c36628b99807881afae0a65bedbb0c9d3812d
Salvador Di Renzo <direnzosalvatore@gmail.com>
2020-12-06 22:01:43 -0400

    add shoulda matchers - validate model/record for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,6 +23,27 @@ RSpec.describe User, type: :model do
     }
   }
 
+  describe "ActiveModel Validations" do
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_presence_of(:last_name) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:username) }
+    it { is_expected.to have_secure_password }
+  end
+
+  describe "ActiveRecord Validations" do
+    it { is_expected.to have_many(:tweets) }
+    it { is_expected.to have_many(:followed_users) }
+    it { is_expected.to have_many(:following_users) }
+    it { is_expected.to have_db_column(:authorization_token).of_type(:string) }
+    it { is_expected.to have_db_index(:authorization_token) }
+    it { is_expected.to have_db_column(:token_lifetime).of_type(:datetime) }
+    it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+    it { is_expected.to have_db_index(:email).unique }
+    it { is_expected.to validate_uniqueness_of(:username).case_insensitive }
+    it { is_expected.to have_db_index(:username).unique }
+  end
+
   describe '::signup' do
     context "when some parameters are not valid" do
       it "doesn't sign up" do
